b9a0337f012e8af69239e542c2fe8bcf32b2bc75
Kurt Hakim <kurthakim@gmail.com>
2021-06-29 01:15:12 +0700

    Create Action creator with redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {    
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH POSTS', payload: response})
+};
